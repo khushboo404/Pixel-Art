fe20796f336723b07927509888e4aa2efbbe935f
khushboo404 <36572021+khushboo404@users.noreply.github.com>
2018-07-03 01:04:10 +0530

    colorpicker

--- js/designs.js
+++ js/designs.js
@@ -1,7 +1,4 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+let colorSelected = "#000";
 
 function makeGrid() {
     //Empty Grid 
@@ -33,13 +30,21 @@ function makeGrid() {
       makeGrid();
     })
   
-  
-  
-  
+//  select color from options
+
+function setColor(color){
+    colorSelected = color;
+}
+    
+
+$("#colorPicker").on("change", ()=>{
+    setColor($("#colorPicker").val());
+});
+
   // Select color input
   
   $( "#pixelCanvas" ).on( "click", "td", function( event ) {
-      $(this).css("background-color", $("#colorPicker").val());
+      $(this).css("background-color", colorSelected);
   });
   
   let down;
@@ -53,7 +58,7 @@ function makeGrid() {
   
   $('table').on('mouseover', 'td', function () {
       if (Down) {
-        var color = $('#colorPicker').val();
+        var color = colorSelected;
           $(this).css( 'background', color);
       }
   });
@@ -62,7 +67,22 @@ function makeGrid() {
   //Reset Grid
   
   function resetGrid(){
-    //event.preventDefault();
-     $("#pixelCanvas").empty();
+    // event.preventDefault();
+    // $("#pixelCanvas").empty();
+    const height = $("#inputHeight").val();
+    const weight = $("#inputWidth").val();
+  
+      
+   for(let i=0;i<height;i++){
+        for(let j=0;j<weight;j++){
+              $(`#${i}_${j}`).css("background-color",'');
+          }
+      }  
+  }
+
+   //New Art Grid
+
+  function newArt(){
+    $("#pixelCanvas").empty();
   }
   
